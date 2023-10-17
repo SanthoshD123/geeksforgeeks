3894cd7faf78069fe535f5f41ce76b8563f49a00
Santhosh D <141542516+SanthoshD123@users.noreply.github.com>
2023-10-17 20:01:39 +0530

    Create Reverse array in groups .java

---
+++ Reverse array in groups .java
@@ -0,0 +1,16 @@
+class Solution {
+    // Function to reverse every sub-array group of size k.
+    void reverseInGroups(ArrayList<Integer> arr, int n, int k) {
+        for (int i = 0; i < n; i += k) {
+            int left = i;
+            int right = Math.min(i + k - 1, n - 1);
+            while (left < right) {
+                int temp = arr.get(left);
+                arr.set(left, arr.get(right));
+                arr.set(right, temp);
+                left++;
+                right--;
+            }
+        }
+    }
+}
