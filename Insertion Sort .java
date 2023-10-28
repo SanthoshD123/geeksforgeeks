c2f2b4953effbf8c727d28c5aeec1052ce9d78d0
Santhosh D <141542516+SanthoshD123@users.noreply.github.com>
2023-10-28 20:43:29 +0530

    Create Insertion Sort .java

---
+++ Insertion Sort .java
@@ -0,0 +1,24 @@
+class Solution
+{
+  // Function to insert the element at index i into its correct position.
+  static void insert(int arr[], int i)
+  {
+      int key = arr[i];
+      int j = i - 1;
+      
+      while (j >= 0 && arr[j] > key) {
+          arr[j + 1] = arr[j];
+          j--;
+      }
+      
+      arr[j + 1] = key;
+  }
+  
+  // Function to sort the array using insertion sort algorithm.
+  public void insertionSort(int arr[], int n)
+  {
+      for (int i = 1; i < n; i++) {
+          insert(arr, i);
+      }
+  }
+}
