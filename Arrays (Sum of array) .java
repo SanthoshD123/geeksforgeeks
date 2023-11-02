db0ce6beec278545c190aa9720eaf42728a12ede
Santhosh D <141542516+SanthoshD123@users.noreply.github.com>
2023-11-02 22:23:07 +0530

    Create Arrays (Sum of array) .java

---
+++ Arrays (Sum of array) .java
@@ -0,0 +1,9 @@
+class Solution {
+    public long getSum(long a[], long n) {
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+}
