2c1e609719357598140bc80f5ed8604d3ec20eea
Santhosh D <141542516+SanthoshD123@users.noreply.github.com>
2023-11-26 21:33:49 +0530

    Create Sort The Array .java

---
+++ Sort The Array .java
@@ -0,0 +1,11 @@
+class Solution 
+{ 
+    int[] sortArr(int[] arr, int n) 
+    { 
+        // Sorting the array in ascending order
+        Arrays.sort(arr);
+        
+        // Returning the sorted array
+        return arr;
+    }
+}
